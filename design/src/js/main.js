652f1a9884414f44f5074a2c007d4c6648f0b3f1
Fernando Poe Bellentani <contato@fernandob.com.br>
2019-06-29 18:02:10 -0300

    Desktop animations done

--- design/src/js/main.js
+++ design/src/js/main.js
@@ -2,9 +2,30 @@
 
 console.log('entra');
 
-var el = document.querySelector('body');
-el.onclick = function() {
-  console.log('clicked!');
-  el.classList.toggle('portfolio--ui'); 
-}
- 
+const elBody = document.querySelector('body');
+// const elUx = document.querySelectorAll('[data-toggle-mod="ux"]');
+// const elUi = document.querySelectorAll('[data-toggle-mod="ui"]');
+
+// elUx.onclick = function() {
+//   console.log('clicked! UX');
+//   elBody.classList.toggle('portfolio--ux'); 
+// }
+// elUi.onclick = function() {
+//   console.log('clicked! UI');
+//   elBody.classList.toggle('portfolio--ui');
+// }
+
+document.addEventListener('click', function (event) {
+  console.log('clique');
+  if (event.target.matches('[data-toggle-mod="ux"]') || event.target.matches('[data-toggle-mod="ux"] span')) {
+    elBody.classList.add('portfolio--ux');
+    elBody.classList.remove('portfolio--ui');
+    console.log('clique ux');
+  };
+  if (event.target.matches('[data-toggle-mod="ui"]') || event.target.matches('[data-toggle-mod="ui"] span')) {
+    elBody.classList.add('portfolio--ui');
+    elBody.classList.remove('portfolio--ux');
+    console.log('clique ui');
+  };
+  event.preventDefault();
+}, false);
